f342a43a0e841ae997905075c8dcd67389d57c70
Student <Student@DESKTOP-UOT3ALI>
2017-05-10 11:50:51 -0700

    changes made

--- 1543/src/LightBulb.java
+++ 1543/src/LightBulb.java
@@ -2,7 +2,23 @@
 public class LightBulb {
 
 	int watts;
+	String type;
+	String color;
 	
 	
+	int getWattage()
+	{
+		return watts;
+	}
+	
+	String getColor(String color)
+	{
+		return color;
+	}
+	
+	String getType(String type)
+	{
+		return type;
+	}
 	
 }
